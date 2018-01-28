802aead3cd8462b89c742cc9445817130c767282
Denis Shvetsov <Denis3200000@yandex.ua>
2018-01-28 22:34:28 +0200

    Initial commit

---
+++ AlmostAdmin/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlmostAdmin.Models
+{
+    public class User : IdentityUser
+    {
+        public int AdminLevel { get; set; }
+    }
+}
